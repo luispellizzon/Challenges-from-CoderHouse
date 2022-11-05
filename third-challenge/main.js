1fa3dc7dfdc053019ea84c1553400ed92ee0b3c4
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-11-05 15:24:07 +0000

    Third Exercises done

---
+++ third-challenge/main.js
@@ -0,0 +1,47 @@
+
+
+/* Exercise 1 Uncomment first two variables and one loop to run the first Exercise*/
+
+// const language = prompt("Qual a sua linguagem de programacao preferida?")
+// const times = parseInt(prompt("de 0 a 10, qual nota voce da para a sua linguagem preferida?"))
+
+/* For */
+// for(let i = 1 ; i <= times; i++){
+//     console.log(`Minha linguagem preferida:❤️ ${language}`)
+// }
+
+/* While */
+// let count = 1
+// while(count <= times){
+//     console.log(`Minha linguage preferida:❤️ ${language}`)
+//     count = count + 1
+// }
+
+
+/* Exercise 2 uncomment the lines bellow to run the exercise 2 & comment exercise 1 complete to not log issues*/
+
+
+/* Do While */
+// let phrase="";
+// let word = prompt(`Vamos escrever uma frase❤️
+// A cada pedido, escreva uma palavra que deseja adicionar a sua frase
+// Se quiser ver a frase final, aperte a tecla ESC ou cancele o prompt`);
+
+// do{
+//     phrase = phrase + " " + word;
+
+//     word = prompt("Proxima palavra");
+
+//     // Checker if user inputs empty string
+//     while(word === ""){
+//         word = alert("Por favor, escreva alguma palavra or cancele o prompt apertando a tecla ESC o apertando o botao de cancelar")
+//         word = prompt("Proxima palavra");
+//     }
+ 
+// }
+// while(word !== null)
+
+// const finalResult = phrase?.trim();
+// console.log(finalResult)
+
+
