36e3c877c5d454a64d9253e54d7ebaad57314684
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-11-11 17:55:31 +0000

    Simulator Done

--- challenge-5/entregavel/main.js
+++ challenge-5/entregavel/main.js
@@ -1,11 +1,15 @@
 
 const App = () => {
 
+    /* Global Variables */
     const VALOR_ANUAL = 180;
+    const WEEKS_PER_YEAR = 52.14
+
+    /* services Object key/value */
     const services = {
         organico: 0.21,
-        reciclavel: 0.21,
-        vidro: 0.21,
+        reciclavel: 0.05,
+        vidro: 0.11,
     };
 
     const { organico, reciclavel, vidro} = services;
@@ -22,38 +26,60 @@ const App = () => {
     Aperte OK para simular o preco anual do nosso servico.
     `);
 
+    /* Helper function to format number up to 2 decimal points*/
+    const parseAndFixDecimal = (number) =>{
+        const numberFixed =  parseFloat((Math.round(number * 100) / 100).toFixed(2))
+        return numberFixed;
+    }
     
     
 
+    /* Put in an array the values from user for each type of waste */
     const readUserInput = () =>{
         let userResults = [];
+
         for(let key in services){
-            let userResult = prompt(`Estimativa de lixo ${key} semanal em kilos.`);
+            let userInput = prompt(`Estimativa de lixo ${key} semanal em kilos (Utilizar numeros inteiros exemplo: 120).`);
     
-            if(userResult === "" || userResult === null){
-                userResult = 0;
-                userResults.push(userResult)
+            if(userInput === "" || userInput === null){
+                userInput = 0;
+                userResults.push(userInput)
                 
             } else {
-                userResult = parseFloat((Math.round(userResult * 100) / 100).toFixed(2));
-                userResults.push(userResult)
+                userInput = parseAndFixDecimal(userInput);
+                userResults.push(userInput)
             }
         }
-
         return userResults;
     }
 
+     /* Calculate total */
+    const calculateTotal = (userValues) =>{
+        const resultValues = userValues.map((value, index) =>{
+    
+            const total = value * Object.values(services)[index] * WEEKS_PER_YEAR;
+            return total
+        }).reduce((prev, curr) => prev + curr, VALOR_ANUAL)
+        
+        const formattedFinalResult = parseAndFixDecimal(resultValues)
+        return formattedFinalResult;
+    }
+
     if(confirmPrompt){
-  
-        const result = readUserInput();
-       console.log(result)
+        const startSimulator = () => {
+            const userWasteKgs = readUserInput();
+            const totalServiceAnnual = calculateTotal(userWasteKgs)
+            
+            alert(` O valor total ao ano sera de: ${totalServiceAnnual} euros`)
+            
+            const reset = confirm("Gostaria de simular novamente?")
+
+            if(reset){
+                startSimulator();
+            }
+        }
+        startSimulator();
    }
 }
     
-
-    
-
-
-
-
 App();
