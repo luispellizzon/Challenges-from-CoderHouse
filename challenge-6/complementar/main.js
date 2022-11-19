7285b873126aafcb6f98d40eb6577ff9da7dc186
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-11-19 20:32:08 +0000

    Regex

--- challenge-6/complementar/main.js
+++ challenge-6/complementar/main.js
@@ -5,7 +5,7 @@ let quantity;
 
 /* -- Checar se usuario inseriu apenas numeros para o tamanho da lista de compras */
 while(!isNumber){
-    quantity = parseInt(prompt("Quantos produtos deseja adicionar a sua basket?\nInsira apenas numeros inteiros.\nEx: 5"));
+    quantity = parseInt(prompt("Quantos produtos deseja adicionar a sua lista?\nInsira apenas numeros inteiros.\nEx: 5"));
     if(!Number.isInteger(quantity)){
         alert("Por favor, insira em numeros inteiros o total de items que deseja adicionar na sua lista")
     } else {
@@ -23,7 +23,15 @@ for(let i = 0; i < quantity ; i++){
 }
 
 /* Mostrar basket do usuario e perguntar se deseja exluir algum*/
-let basket = products.join(", ")
+let lista = products.join(", ")
 
-const isDelete = confirm(`Items adicionados a sua basket:\n${basket}`)
+const isDelete = confirm(`Items adicionados a sua lista:\n${lista}`);
+
+if(!isDelete){
+    alert("Imprima sua lista e aproveite suas compras!")
+} else {
+    let itemsToDelete = prompt(`Quais items deseja excluir da lista?\nLista: ${lista}\nEx: Banana, Pera, Goiaba`)
+    itemsToDelete = itemsToDelete.match(/\w+/gm);
+    console.log(itemsToDelete)
+}
 
