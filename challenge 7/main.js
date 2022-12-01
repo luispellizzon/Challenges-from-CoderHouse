02e566a89ab6a731609bbeee2aefb618153f137e
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-12-01 19:40:51 +0000

    Contact List prototype done

--- challenge 7/main.js
+++ challenge 7/main.js
@@ -1,14 +1,40 @@
 const list = document.querySelector(".list");
+const section = document.getElementById('contactList')
 
 const friends = parseInt(prompt("How many friends would you like to add in your contact list?"))
+ 
+friends ? askDetails(friends) : section.append("No friends added..")
 
- friends != null ? askDetails(friends) : console.log("No friends added")
+function askDetails(num){
 
-
- function askDetails(num){
-
+    let friends = []
     for(let i = 1; i <= num ; i++){
         const name = prompt(`Name of friend ${i}:`)
         const number = prompt(`Phone number of friend ${i}`)
+        const friend = {
+            name,
+            number
+        }
+        friends.push(friend)
     }
+
+    friends = friends.sort((a, b) => a.name.localeCompare(b.name));
+    
+    addToTheList(friends);
+}
+
+ 
+
+ function addToTheList(friends){
+    friends.forEach(friend =>{
+        const li = document.createElement('li')
+        li.innerHTML = `
+            <h2>Name:${friend.name}</h2>
+            <p>Phone number:${friend.number}</p> 
+        `
+        list.appendChild(li)
+    });
  }
+
+
+
