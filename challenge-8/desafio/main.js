bc1e06a223eca3daeefaff72b52fa0aeeba7a7e3
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-12-03 21:15:36 +0000

    App State

--- challenge-8/desafio/main.js
+++ challenge-8/desafio/main.js
@@ -2,18 +2,21 @@ const postTitle = document.getElementById('post-title');
 const postText = document.getElementById('post-text');
 const postForm = document.getElementById('post-form');
 
+const state = {
+    [postTitle.id]: '',
+    [postText.id]: ''
+}
+
 const postDetails = [postTitle, postText]
 postDetails.forEach(input => input.addEventListener('keyup', getDetails))
 
 postForm.addEventListener('submit', submitPost)
 
 function getDetails(e){
-    
-    const info = {[e.target.id]: e.target.value}
-    return info
+    state[e.target.id] = e.target.value
 }
 function submitPost(e){
 e.preventDefault();
 
-
+console.log(state)
 }
